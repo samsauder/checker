e9a7a3f86252e013b71efa216aea5f44ecc5c39a
Sam Sauder <sauders@wwu.edu>
2024-09-30 18:54:59 -0700

    checker.rkt

---
+++ checker/checker.rkt
@@ -0,0 +1,12 @@
+; Sam Sauder
+; 9/30/24
+; Purpose: implementing a basic English sentence validity checker using a 
+;   context-free grammar
+#lang racket
+
+(display "Hello\n")
+
+(define (Q arg0)
+  (+ arg0 1))
+
+(Q 2)
